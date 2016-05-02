572afb816f779ac35721bcbb034bc135360f9e3b
Rachel <rachel.black@flashtalking.com>
2016-05-02 14:20:30 +0100

    priniting out a list of users one page

--- server/controllers/github.js
+++ server/controllers/github.js
@@ -1,10 +1,10 @@
-axios = require('axios');
-
-exports.githubApiTest = function (req, res){
-  const apiTest = axios.get('https://api.github.com/search/users?q=+repos:%3E42+language:javascript+location:leeds')
-    .then(response =>{
-      console.log("response", response);
-      res.send(response)
-    })
-  // console.log("apiTest", apiTest);
-}
+// axios = require('axios');
+//
+// exports.githubApiTest = function (req, res){
+//   const apiTest = axios.get('https://api.github.com/search/users?q=+repos:%3E42+language:javascript+location:leeds')
+//     .then(response =>{
+//       console.log("response", response);
+//       res.send(response)
+//     })
+//   // console.log("apiTest", apiTest);
+// }
--- server/router.js
+++ server/router.js
@@ -2,23 +2,34 @@ const Authentication  = require('./controllers/authentication');
 const passportService = require('./services/passport');
 const passport        = require('passport');
 const Github          = require('./controllers/github')
+const axios           = require('axios');
 
 
 const requireAuth = passport.authenticate('jwt',{ session: false });
 const requireSignin = passport.authenticate('local', { session: false });
 
+
 module.exports = function(app){
     app.get('/', requireAuth, function(req, res){
       res.send({ message: 'super secret code'});
     });
-    app.get('/github', function(req, res){
+
+    // app.get('/github', function(req, res){
+    //   const apiTest = axios.get('https://api.github.com/search/users?q=+repos:%3E42+language:javascript+location:leeds')
+    //     .then(response =>{
+    //       console.log("response", response.data.items[0].login);
+    //       res.send({ message : response.data.items[0].login})
+    //     })
+    //
+    // });
+
+    app.get('/github/test', function(req, res){
       const apiTest = axios.get('https://api.github.com/search/users?q=+repos:%3E42+language:javascript+location:leeds')
         .then(response =>{
           console.log("response", response.data.items[0].login);
-          res.send({ message : response.data.items[0].login})
+          res.send({ message : response.data.items})
         })
-
-    });
+    })
     app.post('/signin', requireSignin, Authentication.signin);
     app.post('/signup',Authentication.signup);
 }
