46b9f4dd36eb88986b6a8b463eeec0e89e4269f7
Rachel <rachel.black@flashtalking.com>
2016-05-23 15:46:46 +0100

    refactor linked to reducer on client

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -86,17 +86,10 @@ export function fetchGithubMessage({location, language}){
         { headers: { authorization: localStorage.getItem('token'),location :location, language: language}
       }).then(response => {
           console.log(">>>", response);
-          // const rawData = response.data.message
-          // rawData.map(function(user){
-          //   axios.get(ROOT_URL + "/github/userdata",
-          // { headers: { authorization: localStorage.getItem('token'), username: user.login}}
-          // ).then(response =>{
-          //       dispatch({
-          //         type: SHOW_USER_DATA,
-          //         payload : response.data
-          //       });
-          //     });
-          // });
+          dispatch({
+             type: SHOW_USER_DATA,
+             payload : response.data
+           });
       });
    }
 }
