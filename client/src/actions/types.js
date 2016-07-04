df1982d6c18a2f1f1e6f576a0cdeb58195a1dca6
Rachel <rachel.black@flashtalking.com>
2016-07-04 17:20:14 +0100

    removing auth on client

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,9 +1,3 @@
-export const AUTH_USER = 'auth_user'
-export const UNAUTH_USER = 'unauth_user'
-export const AUTH_ERROR = 'auth_error'
-export const FETCH_MESSAGE = 'fetch_message'
-
-
 export const GET_USER_DATA = 'get_user_data'
 export const SHOW_USER_DATA = 'show_user_data'
 export const SET_LOCATION_LANG = 'set_location_lang'
