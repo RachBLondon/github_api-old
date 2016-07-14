27fb59795259adf5a2213ebd1ef76203b9c22c64
Rachel <rachel.black@flashtalking.com>
2016-07-14 16:13:33 +0100

    removing mongoose and auth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
-const morgan = require('morgan');
+
 const router = require('./router');
-const mongoose = require('mongoose');
+
 const cors = require('cors');
 
 const app = express();
